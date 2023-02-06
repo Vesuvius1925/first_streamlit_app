72b571198cb1d389e15989143a7af93e90f6c518
Vesuvius1925 <124305351+Vesuvius1925@users.noreply.github.com>
2023-02-07 04:31:48 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,16 +33,9 @@ try:
 except URLError as e:
   streamlit.error()
   
-streamlit.write('The user entered ', fruit_choice)
-
+# streamlit.write('The user entered ', fruit_choice)
 
 # streamlit.text(fruityvice_response)
-
-
-# write your own comment -what does the next line do? 
-
-# write your own comment - what does this do?
-
 
 streamlit.stop()
 
